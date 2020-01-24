17165f285e820a333a9b83cbeccdfa2496ff2fa2
declanvk <dkelly.home@gmail.com>
2020-01-24 04:48:06 +0000

    Deploying to gh-pages from master - 1a8ebe25653338ab7201a63e052ecd6bf1a9fa2c 🚀

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var N=null,E="",T="t",U="u",searchIndex={};
+
+searchIndex["regular"]={"doc":E,"i":[],"p":[]};
+initSearch(searchIndex);addSearchOptions(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["regular"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
