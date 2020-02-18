947b007862dde7401a1878f6d6b68a79f9e2cd90
declanvk <dkelly.home@gmail.com>
2020-02-18 17:25:22 +0000

    Deploying to gh-pages from master - d89242c7f4c66d0ad1f97cc71baaacec912a4d35 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["regular"] = {"name":"","dirs":[{"name":"util","files":["bit_set.rs","cartesian_product.rs","join_iter.rs","set_partition.rs","step.rs","storage_int.rs","vec_set.rs"]}],"files":["accept.rs","alphabet.rs","dfa.rs","error.rs","lib.rs","util.rs"]};
+sourcesIndex["regular"] = {"name":"","dirs":[{"name":"dfa","files":["operations.rs"]},{"name":"util","files":["bit_set.rs","cartesian_product.rs","join_iter.rs","set_partition.rs","step.rs","storage_int.rs","vec_set.rs"]}],"files":["accept.rs","alphabet.rs","dfa.rs","error.rs","lib.rs","util.rs"]};
 createSourceSidebar();
